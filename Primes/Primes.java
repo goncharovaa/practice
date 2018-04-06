86cd90759b9d73eba5227ea194bc7a1cdaa8fc88
goncharovaa <goncharovakate1@gmail.com>
2018-04-06 13:26:18 +0300

    Простые числа

---
+++ Primes/Primes.java
@@ -0,0 +1,19 @@
+
+// ������� �����
+class Primes{
+	
+	public static void main(String[] args) {
+		for(int i=2; i<=1000; i++)
+			if (isPrime(i))
+				System.out.printf("%d ", i);		
+	}
+	
+	public static boolean isPrime(int n) {
+		for(int i=2;i<n/2;i++) 
+			if(n%i==0)
+				return false;
+		return true;
+	}
+	
+	
+}
